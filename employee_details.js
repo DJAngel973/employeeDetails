daac6a5c8bd3c2df59dc12be22db5632c9a11f8f
DJAngel973 <angel.djuancho@gmail.com>
2025-08-30 21:27:46 -0500

    feat: Adds functions to calculate employee salaries, display details by department, and display details by employee ID.

--- employee_details.js
+++ employee_details.js
@@ -6,5 +6,26 @@ const employees = [
 // Function to display all employees
 function displayEmployees() {
     const totalEmployees = employees.map((employee, index) => `<p>${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`).join('');
-        document.getElementById('employeesDetails').innerHTML = totalEmployees;
+    document.getElementById('employeesDetails').innerHTML = totalEmployees;
+}
+// Function to calculate employees total salaries
+function calculateTotalSalaries(){
+    const totalSalaries = employees.reduce((acc, employee) => acc + employee.salary, 0);
+    alert(`Total Salaries: $${totalSalaries}`);
+}
+// Function to display employees details based on department such as the HR department
+function displayHREmployees() {
+    const hrEmployees = employees.filter(employee => employee.department === 'HR');
+    const hrEmployeesDisplay = hrEmployees.map((employee, index) => `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`).join('');
+    document.getElementById(`employeesDetails`).innerHTML = hrEmployeesDisplay;
+}
+// Function to display employees details based on ID
+function findEmployeeById(employeeId) {
+    const foundEmployee = employees.find(employee => employee.id === employeeId);
+    if (foundEmployee) {
+        document.getElementById('employeesDetails').innerHTML = `<p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.salary}</p>`;
+    }
+    else{
+        document.getElementById(`employeesDetails`).innerHTML = 'No employee has been found with this ID';
+    }
 }
